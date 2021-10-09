13c0862bf7147049b0305981701c884fb8fecc6d
GokulnathP <4gokul2000@gmail.com>
2021-10-09 11:46:00 +0530

    Update dependencies and moving to yarn

--- webpack.config.js
+++ webpack.config.js
@@ -7,23 +7,13 @@ module.exports = merge(common, {
     mode: 'development',
     devtool: 'source-map',
     devServer: {
-        contentBase: './dist',
+        static: {
+            directory: './dist'
+        },
         open: true,
         port: 3000,
-        clientLogLevel: 'debug',
-        noInfo: true,
-        host: '0.0.0.0',
-        before: function (app) {
-            app.use('/', function (req, res, next) {
-                if (count === 0) {
-                    console.log("Local:           " + req.protocol + '://' + req.get('host'));
-                    require('dns').lookup(require('os').hostname(), function (err, add) {
-                        console.log("On Your Network: " + req.protocol + '://' + add + ':3000');
-                    })
-                    count++;
-                }
-                next()
-            });
-        },
+        client: {
+            logging: 'info'
+        }
     }
 });
