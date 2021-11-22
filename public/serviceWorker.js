99236f1709577dbe1a885515b5b4468af1de9a74
GokulnathP <4gokul2000@gmail.com>
2021-11-22 18:14:04 +0530

    Update service worker to staleWhileRevalidate caching strategy

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -12,45 +12,72 @@ const STATIC_REQUESTS = [
     '/favicon.ico'
 ];
 
-self.addEventListener('install', function (event) {
-    console.log('[Service worker] installing....');
+self.addEventListener('install', (event) => {
     self.skipWaiting();
-    event.waitUntil(
-        caches.open(STATIC_CACHE_NAME)
-            .then(function (cache) {
-                console.log('[Service worker] caching...');
-                return cache.addAll(STATIC_REQUESTS);
-            })
-    );
+    event.waitUntil(cacheStaticRequests());
 });
 
-self.addEventListener('activate', function (event) {
-    console.log('[Service worker] activating...');
-    event.waitUntil(
-        caches.keys()
-            .then(function (keyList) {
-                return Promise.all(keyList.map(function (key) {
-                    if (!(key === STATIC_CACHE_NAME || key === DYNAMIC_CACHE_NAME)) {
-                        return caches.delete(key);
-                    }
-                }));
-            })
-    );
+self.addEventListener('activate', (event) => {
+    event.waitUntil(cleanUpCaches());
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                return response;
-            } else {
-                return fetch(event.request).then(function (res) {
-                    caches.open(DYNAMIC_CACHE_NAME).then(function (cache) {
-                        cache.put(event.request.url, res.clone());
-                        return res;
-                    });
-                });
+self.addEventListener('fetch', (event) => {
+    event.respondWith(staleWhileRevalidate(event));
+});
+
+const cacheStaticRequests = () => {
+    return caches.open(STATIC_CACHE_NAME).then((cache) => {
+        return cache.addAll(STATIC_REQUESTS);
+    });
+};
+
+const cleanUpCaches = () => {
+    return caches.keys().then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+            if (!(key === STATIC_CACHE_NAME || key === DYNAMIC_CACHE_NAME)) {
+                return caches.delete(key);
             }
-        })
-    );
-});
+        }));
+    });
+};
+
+const staleWhileRevalidate = (event) => {
+    return staleWhileRevalidateFromStaticCache(event)
+        .then((staticResponse) => {
+            if (staticResponse) {
+                return staticResponse;
+            }
+            return staleWhileRevalidateFromDynamicCache(event);
+        });
+};
+
+const staleWhileRevalidateFromStaticCache = (event) => {
+    return caches.open(STATIC_CACHE_NAME).then((staticCache) => {
+        return staticCache.match(event.request).then((staticResponse) => {
+            if (staticResponse) {
+                fetch(event.request).then((networkResponse) => {
+                    staticCache.put(event.request.url, networkResponse.clone());
+                })
+                return staticResponse;
+            }
+            return undefined;
+        });
+    });
+};
+
+const staleWhileRevalidateFromDynamicCache = (event) => {
+    return caches.open(DYNAMIC_CACHE_NAME).then((dynamicCache) => {
+        return dynamicCache.match(event.request).then((dynamicResponse) => {
+            if (dynamicResponse) {
+                fetch(event.request).then((networkResponse) => {
+                    dynamicCache.put(event.request, networkResponse.clone());
+                });
+                return dynamicResponse;
+            }
+            return fetch(event.request).then((networkResponse) => {
+                dynamicCache.put(event.request, networkResponse.clone());
+                return networkResponse;
+            });
+        });
+    });
+};
